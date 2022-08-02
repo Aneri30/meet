5effeeb3b1df1d301d0a5d584eca52ebc932923e
Aneri3112 <aneri.pate@hotmail.com>
2022-08-02 23:48:37 +0200

    add styling

--- src/Event.js
+++ src/Event.js
@@ -19,11 +19,10 @@ class Event extends Component {
                 <h4 className='event-title'>{this.props.event.summary}</h4>
                 <p className='event-time'>{this.props.event.start.dateTime}</p>
                 <p className='event-location'>{this.props.event.location}</p>
-                <a href={this.props.event.htmlLink} rel="noreferrer" target="_blank">
-                    See details on Google Calendar
-                </a>
                 {!this.state.showDetails && <button className='showDetails-button' onClick={this.handleShowDetails}>Show details</button>}
-                {this.state.showDetails && <p className='event-details'>{this.props.event.description}</p>}
+                {this.state.showDetails && <p className='event-details'>{this.props.event.description}, <a href={this.props.event.htmlLink} rel="noreferrer" target="_blank">
+                    See details on Google Calendar
+                </a> </p>}
                 {this.state.showDetails && <button className='hideDetails-button' onClick={this.handleHideDetails}>Hide details</button>}
             </div>
         )
